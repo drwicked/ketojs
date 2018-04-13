2a6abe4c0f0434a2f99b10be10701b2e1fa39b57
Jeff Printy <yeffrey@gmail.com>
2018-04-13 01:52:06 +0000

    pretty good

--- server.js
+++ server.js
@@ -2,17 +2,36 @@ const server = require('./ketosis-js')
 const ketoStatic = require('./keto-static')
 const cookies = require('./keto-cookies');
 const pug = require('./keto-pug')
+const fs = require('fs')
 
 const app = module.app = server()
-
-app.listen("localhost:8000")
+const http = module.app = server()
 
 const pugDir = pug({path: `${app.path}/site/pug/`})
 const staticServer = ketoStatic({ path: app.path+'/site/static/' })
 
-app.header(cookies);
-app.header(pugDir);
-app.view('file', staticServer);
-app.get('/', $ => {
-  $.render('index.pug');
+const httpApps = {}
+const apps = {}
+
+const sites = [
+  'writeordie.social'
+]
+sites.forEach(site => {
+  apps[site] = module.app = server()
+  httpApps[site] = module.app = server()
+  httpApps[site].listen(`http://${site}`)
+  apps[site].get('/', $ => {
+    $.redirect(`https://${site}`);
+  })
+  apps[site].listen(`https://${site}`, {
+     cert: fs.readFileSync(`/etc/letsencrypt/live/${site}/cert.pem`),
+     key: fs.readFileSync(`/etc/letsencrypt/live/${site}/privkey.pem`)
+  })
+
+  apps[site].header(cookies);
+  apps[site].header(pugDir);
+  apps[site].view('file', staticServer);
+  apps[site].get('/', $ => {
+    $.render('index.pug');
+  })
 })
