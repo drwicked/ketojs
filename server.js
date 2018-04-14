409df8236b94505cf0fb796eaae1fed3519473dc
Jeff Printy <yeffrey@gmail.com>
2018-04-13 19:31:52 -0500

    working

--- server.js
+++ server.js
@@ -5,34 +5,50 @@ const pug = require('./keto-pug')
 const fs = require('fs')
 
 const app = module.app = server()
-const http = module.app = server()
 
-const pugDir = pug({path: `${app.path}/site/pug/`})
-const staticServer = ketoStatic({ path: app.path+'/site/static/' })
 
-const httpApps = {}
-const apps = {}
+const http = {}
+const https = {}
 
 const sites = [
   'writeordie.social'
 ]
-console.log('process', process.env.NODE_ENV);
+let stPort = 8000;
 sites.forEach(site => {
-  apps[site] = module.app = server()
-  httpApps[site] = module.app = server()
-  httpApps[site].listen(`http://${site}`)
-  apps[site].get('/', $ => {
-    $.redirect(`https://${site}`);
-  })
-  apps[site].listen(`https://${site}`, {
-     cert: fs.readFileSync(`/etc/letsencrypt/live/${site}/cert.pem`),
-     key: fs.readFileSync(`/etc/letsencrypt/live/${site}/privkey.pem`)
-  })
+  http[site] = module.app = server()
+  http[site].header(cookies)
+  https[site] = module.app = server()
+  https[site].header(cookies)
+  const sitePath = `${app.path}/sites/${site}`;
+  if (process.env.NODE_ENV === 'development') {
+    http[site].listen(`http://localhost:${stPort}`)
+    http[site].header(
+      pug({path: `${sitePath}/pug/`})
+    );
+    http[site].view('file',
+      ketoStatic({ path: `${sitePath}/static/` })
+    );
+    stPort += 1
+  } else {
+    http[site].listen(`http://${site}`)
+    http[site].get('/', $ => {
+      $.redirect(`https://${site}`)
+    })
+    https[site].listen(`https://${site}`, {
+      cert: fs.readFileSync(`/etc/letsencrypt/live/${site}/cert.pem`),
+      key: fs.readFileSync(`/etc/letsencrypt/live/${site}/privkey.pem`)
+    })
 
-  apps[site].header(cookies);
-  apps[site].header(pugDir);
-  apps[site].view('file', staticServer);
-  apps[site].get('/', $ => {
-    $.render('index.pug');
-  })
+    https[site].header(
+      pug({path: `${sitePath}/pug/`})
+    );
+    https[site].view('file',
+      ketoStatic({ path: `${sitePath}/static/` })
+    );
+    https[site].get('/', $ => {
+      $.render('index.pug');
+    })
+  }
+
+
 })
